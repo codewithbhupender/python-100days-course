a20f5fa6de53fd71c3114ed3bfd8b795abc859c0
CodeWithBhupender <codewithbhupender@gmail.com>
2024-03-20 12:11:19 +0530

    string methods in python

--- inputinpython.py
+++ inputinpython.py
@@ -1,6 +1,3 @@
-a=input("Enter the value of a :")
-print("Value of a is :",a)
-b=input("Enter the value of b :")
-print("Value of b is :",b)
-
-print("Value of a+b is ",int(a)+int(b))
+a=input("Enter the value of a: ")
+b=input("Enter the value of a: ")
+print(int(a)+int(b))
---
+++ stringmethods.py
@@ -0,0 +1,13 @@
+a="Devin"
+#slicing string
+# print(a[0:5])
+
+#length of string
+print("Lenght of a is :",len(a))
+#convert string to uppercase
+print(a.upper())
+print(a.lower())
+b="youhow are yous"
+print(b.count('are'))
+
+print(b.startswith('you'))
