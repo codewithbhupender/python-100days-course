bb253771e6f738856a96b0501b4d41e9764f2bb3
CodeWithBhupender <codewithbhupender@gmail.com>
2024-03-22 17:50:23 +0530

    Conditional OPerators and if else in python

---
+++ conditionalOperators.py
@@ -0,0 +1,21 @@
+#Conditional Operators in Python :
+#<
+#>
+#<=
+#>=
+#==
+#!=
+a=10
+b=20
+
+if a<b and a>10:
+    print("a is greater than b")
+    print("a is greater")
+elif a>=10:
+    print("a is greater then 10 ")
+else:
+    print("b is greater than a")
+    print("b is greater")   
+print("Am i out of if else block?")
+
+
